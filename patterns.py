3272d266db995f6fb9810031885b66f2247ea81e
Zo Hasina MEMINIAINA <zo@constellation-group.local>
2023-08-29 18:25:35 +0300

    First test checking patterns

--- patterns.py
+++ patterns.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from fill_variables import *
 import docx
+import re
 
 #Get number of questions ----------------------------------------------------------------------------------------
 def get_nb_questions():
@@ -16,20 +17,36 @@ def get_nb_questions():
 # --------------------------------------------------------------------------------------------------------------
 
 #Get patterns --------------------------------------------------------------------------------------------------
-def get_patterns():
+def get_patterns(subject):
     pattern_Debut = []
-    pattern_End = []
+    pattern_End = []  
+    pattern_point = ["points)","point)","pts)","pt)"]
+
     nb_Questions = get_nb_questions()
+    pattern_numero = [[f"{num}\)",f"{num}/",f"Q{num}",f"QUESTION {num}",f"QUESTION{num}"] for num in range(nb_Questions)]
+
+    is_inside_paragraph = False
+    if subject.endswith(".docx"):
+        file = docx.Document(subject)
+        for paragraph in file.paragraphs:
+            for i in range(nb_Questions):
+                for j in range(len(pattern_numero[i])):
+                    if re.match(pattern_numero[i][j], paragraph.text):
+                        is_inside_paragraph = True
+
+    return is_inside_paragraph
 
     #for i in range(nb_Questions):
 
 # --------------------------------------------------------------------------------------------------------------
 
-doc = docx.Document("reference_answer/" + get_file("reference_answer/"))
+#doc = docx.Document("reference_answer/" + get_file("reference_answer/"))
+#
+##nb = get_nb_questions()
+#document = ""
+#for i in range(len(doc.paragraphs)):
+#    document += (doc.paragraphs[i].text + "\n")
+#
+#print(document)
 
-#nb = get_nb_questions()
-document = ""
-for i in range(len(doc.paragraphs)):
-    document += (doc.paragraphs[i].text + "\n")
-
-print(document)
+print(get_patterns("reference_answer/" + get_file("reference_answer/")))
